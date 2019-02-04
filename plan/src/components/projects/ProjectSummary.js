bd3509f3e75953c9833e58def14e72a4f0174256
mohamedsubhi <mohamedsubhibouchi@gmail.com>
2019-02-04 12:50:28 +0100

    dashboard, projectlist, project summary templates created. app switch route

---
+++ plan/src/components/projects/ProjectSummary.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function ProjectSummary() {
+  return (
+    <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+            <span className="card-title"></span>
+            <p>posted by</p>
+            <p className="grey-text">01.01.2019</p>
+        </div>
+    </div>
+  )
+}
