68fc98f866051c8227404c8c5ee38d096d772ab7
mohamedsubhi <mohamedsubhibouchi@gmail.com>
2019-02-04 12:36:48 +0100

    navbar, signedinlink and signedoutlnks created

---
+++ plan/src/components/layout/SignedInLinks.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+
+export default function SignedInLinks() {
+  return (
+    <ul className="right">
+        <li>
+            <NavLink to='/'>new project</NavLink>
+        </li>
+        <li>
+            <NavLink to='/'>logout</NavLink>
+        </li>
+        <li>
+            <NavLink to='/' className='btn btn-floating pink lighten-1'>MM</NavLink>
+        </li>
+    </ul>
+  )
+}
