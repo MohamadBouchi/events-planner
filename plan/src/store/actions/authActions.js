02bbdd39e7943a7c2816e267e8aaf911c481564e
Mohamed Bouchi <mohamedsubhibouchi@gmail.com>
2019-02-05 21:23:59 +0100

    signIn authAction authReducer

---
+++ plan/src/store/actions/authActions.js
@@ -0,0 +1,14 @@
+export const signIn = (credentials) => {
+    console.log(credentials.password);
+    return (dispatch, getState, {getFirebase}) => {
+        const firebase = getFirebase();
+        firebase.auth().signInWithEmailAndPassword(
+            credentials.email,
+            credentials.password
+        ).then(() => {
+            dispatch({type: 'LOGIN_SUCCESS'})
+        }).catch((err) => {
+            dispatch({type: 'LOGIN_ERROR', err})
+        })
+    }
+}
