04964f281d3d977456444d8989ffe8dc00d59f7f
Mohamed Bouchi <mohamedsubhibouchi@gmail.com>
2019-02-05 21:36:31 +0100

    signout

--- plan/src/store/reducers/authReducer.js
+++ plan/src/store/reducers/authReducer.js
@@ -9,7 +9,10 @@ const authReducer = (state = initState, action) => {
             return { ...state, authError: 'Login failed' };
         case 'LOGIN_SUCCESS':
             console.log('login success');
-            return { ...state, authError: null}
+            return { ...state, authError: null};
+        case 'SIGNOUT_SUCCESS':
+            console.log('sign out success');
+            return state;
         default: 
             return state;
     }   
